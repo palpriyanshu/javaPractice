a8397a4f920ddf74571282a481599669652fe71b
priyanshu <priyanshu@priyanshus-MacBook-Pro.local>
2020-10-12 11:58:28 +0530

    added  HCF.java

---
+++ HCF.java
@@ -0,0 +1,20 @@
+public class HCF{
+  public static void main(String[] args){
+    final int num1 = Integer.parseInt(args[0]);
+    final int num2 = Integer.parseInt(args[1]);
+    int smallerNum = num1;
+    int largerNum = num2;
+    if(num1 > num2){
+      smallerNum = num2;
+      largerNum = num1;
+    }
+
+    while(smallerNum != 0){
+      int hcf = smallerNum;
+      smallerNum = largerNum % smallerNum;
+      largerNum = hcf;
+    }
+    System.out.println("HCF of " + num1 + " and " + num2 + " is " + largerNum);
+
+  }
+}
